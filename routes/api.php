9eaa8de43468546b8ec96581a08f9d37991e082c
Arwa Mohammed <arwamohmd.32@gmail.com>
2025-04-12 18:58:14 +0200

    Implement API authentication with Sanctum; add PostController and resources for posts

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\API\PostController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('posts', [PostController::class, 'index'])->middleware('auth:sanctum');
+Route::get('posts/{post}', [PostController::class, 'show'])->middleware('auth:sanctum');
+
+Route::post('posts', [PostController::class, 'store'])->middleware('auth:sanctum');
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
